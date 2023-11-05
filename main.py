7455324361ea37e8d81b1add0fde25c7b81b503d
mtake <mtake88@gmail.com>
2023-11-05 12:26:09 +0900

    ・最後にファイルをトップに移動させるように変更。 ・コメントを充実させた。

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ from collections import defaultdict
 import tkinter as tk
 from tkinter import filedialog, messagebox, ttk
 
+
 def choose_deletion_criteria():
     """
-    ユーザーに削除の基準を選択させる関数。
-    ラジオボタンを使って3つの基準から選ぶことができます。
+    ユーザーに削除基準を選択させる関数。
+    ラジオボタンを使用して、削除の基準を選択させます。
     """
+
+    # 選択した基準を取得してウィンドウを閉じる関数
     def submit_choice():
         user_choice.set(v.get())
         choice_window.destroy()
 
+    # キャンセルしてウィンドウを閉じる関数
     def cancel_and_exit():
         user_choice.set(-1)
         choice_window.destroy()
 
-    # 選択肢
+    # 削除基準のオプション
     options = [
         "同じ名前のファイルを１つだけ残す",
         "同じサイズのファイルを１つだけ残す",
@@ -29,49 +33,64 @@ def choose_deletion_criteria():
     v = tk.IntVar()
     v.set(0)
 
-    # ラジオボタンの設定
+    # ラジオボタンの表示
     for i, option in enumerate(options):
         ttk.Radiobutton(choice_window, text=option, variable=v, value=i).pack(pady=5, padx=10, anchor='w')
 
-    # ボタンの設定
+    # サブミットボタンとキャンセルボタンの表示
     ttk.Button(choice_window, text="Submit", command=submit_choice).pack(pady=10, side='left', padx=10)
     ttk.Button(choice_window, text="Cancel", command=cancel_and_exit).pack(pady=10, side='right', padx=10)
 
     user_choice = tk.IntVar(value=-2)
+
+    # ウィンドウを中央に配置
+    choice_window.update_idletasks()
+    width = choice_window.winfo_width()
+    height = choice_window.winfo_height()
+    x = (choice_window.winfo_screenwidth() // 2) - (width // 2)
+    y = (choice_window.winfo_screenheight() // 2) - (height // 2)
+    choice_window.geometry(f'{width}x{height}+{x}+{y}')
+
     choice_window.mainloop()
 
     return user_choice.get()
 
+
 def select_directory():
     """
-    ユーザーにフォルダを選択させる関数。
-    選択したフォルダのパスを返します。
+    ユーザーにディレクトリを選択させるダイアログを表示。
+    選択したディレクトリのパスを返す。
     """
+
     root = tk.Tk()
     root.withdraw()
     folder_selected = filedialog.askdirectory()
     root.destroy()
     return folder_selected
 
+
 def delete_duplicate_files(target_directory, criteria):
     """
-    重複するファイルを削除する関数。
-    criteriaに応じて、名前やサイズでの重複を判定します。
+    指定ディレクトリ内の重複ファイルを指定の基準に基づいて削除。
+    :param target_directory: ターゲットとなるディレクトリのパス
+    :param criteria: 削除の基準 (0, 1, or 2)
     """
+
     if not os.path.exists(target_directory):
         print(f"ディレクトリ {target_directory} が存在しません。")
         return
 
     collection = defaultdict(list)
 
-    # 指定されたディレクトリ内のすべてのファイルを調べる
+    # ファイルの収集
     for root, _, files in os.walk(target_directory):
         for file in files:
             filepath = os.path.join(root, file)
             if os.path.isfile(filepath):
                 key = file if criteria in [0, 2] else os.path.getsize(filepath)
                 collection[key].append(filepath)
-    # 重複するファイルを削除する
+
+    # 重複ファイルの削除
     for files in collection.values():
         if len(files) > 1:
             files_to_delete = files[1:] if criteria != 2 else [f for f in files[1:] if os.path.getsize(f) == os.path.getsize(files[0])]
@@ -82,23 +101,53 @@ def delete_duplicate_files(target_directory, criteria):
                 except Exception as e:
                     print(f"ファイル {filepath} を削除中にエラーが発生しました: {e}")
 
+
 def show_end_dialog():
-    """ 処理終了時に"END"と表示するダイアログを出す関数 """
+    """ 処理の終了をユーザーに通知するダイアログを表示 """
+
     root = tk.Tk()
     root.withdraw()
     messagebox.showinfo("情報", "END")
     root.destroy()
 
+
+def move_files_to_top(target_directory):
+    """
+    指定ディレクトリ内のファイルをディレクトリのトップレベルに移動。
+    同名のファイルが存在する場合は連番を付けて重複を避ける。
+    :param target_directory: ターゲットとなるディレクトリのパス
+    """
+
+    for root, _, files in os.walk(target_directory):
+        for file in files:
+            original_path = os.path.join(root, file)
+            target_path = os.path.join(target_directory, file)
+
+            if os.path.abspath(original_path) == os.path.abspath(target_path):
+                continue
+
+            # 同名ファイルの処理
+            counter = 1
+            while os.path.exists(target_path):
+                base, ext = os.path.splitext(file)
+                file = f"{base}({counter}){ext}"
+                target_path = os.path.join(target_directory, file)
+                counter += 1
+
+            os.rename(original_path, target_path)
+
+
 if __name__ == "__main__":
-    # 削除基準を選択
+    # 削除基準の選択
     criteria = choose_deletion_criteria()
     if criteria != -1 and criteria != -2:
-        # フォルダを選択
+        # ディレクトリの選択
         target_directory = select_directory()
         if target_directory:
-            # 重複するファイルを削除
             delete_duplicate_files(target_directory, criteria)
-            # 処理終了のダイアログを表示
+            # ファイルの移動
+            move_files_to_top(target_directory)
+            # 終了通知
             show_end_dialog()
         else:
             print("フォルダが選択されませんでした。")
